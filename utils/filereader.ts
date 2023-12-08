777f42832a074955f3f37e5e57bef7ce270dd40c
JannisK89 <jannis.karanikis@gmail.com>
2023-12-08 10:27:09 +0100

    rm old files

--- utils/filereader.ts
+++
@@ -1,6 +0,0 @@
-export const getInputAsList = async (path: string): Promise<string[]> => {
-  const text = await Bun.file(path).text()
-  const lines = text.split('\n')
-  lines.pop()
-  return lines
-}
